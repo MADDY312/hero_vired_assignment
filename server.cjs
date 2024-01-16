8403b903a0b1cb5fae5c8b70390ac659fa8f2e9d
vasu gupta <vasugupta3dec@gmail.com>
2024-01-16 11:30:24 +0530

    modified server_2.cjs

--- server.cjs
+++ server.cjs
@@ -4,26 +4,24 @@ const programRoutes = require('./src/routes/programRoutes');
 const authRoutes = require('./src/routes/authRoutes');
 
 const path = require('path');
-const __dirname=path.resolve();
-
 const cors = require("cors");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
 app.use('/auth', authRoutes); // Login route
 app.use('/programs', programRoutes);
-app.use(express.static(path.join(__dirname,"./client/build")));
-app.get("*",(req,res)=>{
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", (req, res) => {
   res.sendFile(
-    path.join(__dirname,"./client/build/index.html"),
-    function(err){
-      res.status(500).send(err)
+    path.join(__dirname, "./client/build/index.html"),
+    function(err) {
+      res.status(500).send(err);
     }
-  )
-})
+  );
+});
 
 const PORT = process.env.PORT || 3000;
 
